9e75cc7a2fa7dbccd782a0c1627692693feea051
Nikita Utkin <shockck84@gmail.com>
2025-04-21 22:40:26 +0900

    Add AudioPlayerModel

---
+++ TinyMuse/AudioPlayerModel.swift
@@ -0,0 +1,54 @@
+import Foundation
+import AVFAudio
+
+class AudioPlayerModel: ObservableObject {
+    @Published var isPlaying: Bool = false
+    @Published var progress: Double = 0.0
+    @Published var errorText: String?
+    
+    private var player: AVAudioPlayer?
+    private var timer: Timer?
+    
+    init(fileURL: URL?) {
+        if let url = fileURL {
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+            } catch {
+                errorText = error.localizedDescription
+            }
+        }
+        startTimer()
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            guard let player = self.player else { return }
+            self.progress = player.duration == 0 ? 0 : player.currentTime / player.duration
+        }
+    }
+    
+    func togglePlay() {
+        guard let player = player else { return }
+        isPlaying.toggle()
+        
+        if isPlaying {
+            player.play()
+        } else {
+            player.pause()
+        }
+    }
+    
+    func setProgress(_ newValue: Double) {
+        guard let player = player else { return }
+        player.currentTime = newValue * player.duration
+        self.progress = newValue
+    }
+    
+    func currentTime() -> TimeInterval? {
+        player?.currentTime
+    }
+    
+    func totalTime() -> TimeInterval? {
+        player?.duration
+    }
+}
